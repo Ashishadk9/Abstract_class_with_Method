019ef4c2a522a253a83fbcbb8fc3cd7fc4c1963f
Ashishadk9 <aashishadk82@gmail.com>
2025-05-08 21:31:38 +0545

    Add files via upload

---
+++ Bus.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Bus here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+class Bus extends Vehicle
+{
+    @Override
+    void wheel(){
+        System.out.println("A Bus has Six wheel.");
+    }
+}
---
+++ Vehicle.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Bus here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+abstract class Vehicle
+{
+  abstract void wheel();
+  void door(){
+      System.out.println("This Vehicle has door.");
+  }
+}
